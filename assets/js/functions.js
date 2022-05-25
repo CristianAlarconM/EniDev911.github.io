70db088059606a0e78ed57f000b909aa956ffd37
EniDev911 <enidev911@gmail.com>
2022-05-24 21:17:12 -0400

    update playsound function

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -100,9 +100,10 @@ function playSound(filename = relativePathMedia + "lofi-beat-chill") {
       * Plays a sound using the HTML5.
       * @param {string} filename The name of the file.!
       */
-    var mp3Source = '<source src="' + filename + '.mp3" type="audio/mpeg">';
-    var oggSource = '<source src="' + filename + '.ogg" type="audio/ogg">';
-    var embedSource = '<embed hidden"true" autostart="true" loop="false" src="' + filename + '.mp3>"';
+    this.filename = relativePathMedia + filename
+    var mp3Source = '<source src="' + this.filename + '.mp3" type="audio/mpeg">';
+    var oggSource = '<source src="' + this.filename + '.ogg" type="audio/ogg">';
+    var embedSource = '<embed hidden"true" autostart="true" loop="false" src="' + this.filename + '.mp3>"';
     document.getElementById("sound").innerHTML = '<audio autoplay="autoplay" controls>' + mp3Source + oggSource + embedSource + '</audio>';
 }
 
